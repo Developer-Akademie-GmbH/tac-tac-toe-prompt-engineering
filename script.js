b29c4e1af1aff0e64e9f0465dc4861ef11e46ce1
Junus Ergin <junus91@googlemail.com>
2023-04-25 10:49:52 +0200

    06 - Gewinner ermitteln

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ let fields = [
     null,
 ];
 
+const WINNING_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 6], // diagonal
+];
+
 let currentPlayer = 'circle';
 
 
@@ -48,8 +54,28 @@ function handleClick(cell, index) {
         cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVG() : generateCrossSVG();
         cell.onclick = null;
         currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+
+        if (isGameFinished()) {
+            const winCombination = getWinningCombination();
+            drawWinningLine(winCombination);
+        }
     }
 }
+
+function isGameFinished() {
+    return fields.every((field) => field !== null) || getWinningCombination() !== null;
+}
+
+function getWinningCombination() {
+    for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+        const [a, b, c] = WINNING_COMBINATIONS[i];
+        if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
+            return WINNING_COMBINATIONS[i];
+        }
+    }
+    return null;
+}
+
 
 function generateCircleSVG() {
     const color = '#00B0EF';
@@ -88,3 +114,28 @@ function generateCrossSVG() {
 }
 
 
+
+function drawWinningLine(combination) {
+    const lineColor = '#ffffff';
+    const lineWidth = 5;
+
+    const startCell = document.querySelectorAll(`td`)[combination[0]];
+    const endCell = document.querySelectorAll(`td`)[combination[2]];
+    const startRect = startCell.getBoundingClientRect();
+    const endRect = endCell.getBoundingClientRect();
+
+    const lineLength = Math.sqrt(
+        Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
+    );
+    const lineAngle = Math.atan2(endRect.top - startRect.top, endRect.left - startRect.left);
+
+    const line = document.createElement('div');
+    line.style.position = 'absolute';
+    line.style.width = `${lineLength}px`;
+    line.style.height = `${lineWidth}px`;
+    line.style.backgroundColor = lineColor;
+    line.style.top = `${ startRect.top + startRect.height / 2 - lineWidth / 2 } px`;
+    line.style.left = `${ startRect.left + startRect.width / 2 } px`;
+    line.style.transform = `rotate(${ lineAngle }rad)`;
+    document.getElementById('content').appendChild(line);
+}
